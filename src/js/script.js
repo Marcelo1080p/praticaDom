47b7be2e1b6c03ca8ecadb2c97470fab25ea2b38
Marcelo1080p <juniooor.2015@gmail.com>
2022-11-22 21:23:34 -0300

    criando butto de remover elementos

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,33 @@
 const arrayProdutos = []
-
+let id = 0
 function cadastrarProdutos(event) {
     const produto = event.children.produtos.value
     const valorProduto = event.children.valor.value
-    const produtoValor = {produto, valorProduto}
+    const produtoValor = {id,produto, valorProduto}
     arrayProdutos.push(produtoValor)
+    id++
+   
     document.querySelector('input[name="produtos"]').value = ""
     document.getElementById("valor").value = ""
     
-    console.log(arrayProdutos)
+    console.log(id,arrayProdutos)
 }
 
 function verCarrinho() {
     const pProduto = document.createElement('p')
     const vProduto = document.createElement('p')
+    const btn = document.createElement('button')
+    btn.type = "button"
+    btn.innerText = "Remover"
     arrayProdutos.map((devolverValor) => {
         pProduto.innerText = devolverValor.produto
         vProduto.innerText = devolverValor.valorProduto
-        document.querySelector('.carrinho').append(pProduto,vProduto)
+        document.querySelector('.carrinho').append(pProduto,vProduto, btn)
         
     })
+   btn.addEventListener('click', () => {
+        document.querySelector('.carrinho').removeChild(arrayProdutos[arrayProdutos.length -1])
+   })
+    
     
 }
